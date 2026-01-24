b4397ea3ffcb7847c942ea9021de3cc248aab4af
Parth Malavia <parthmalavia286@gmail.com>
2026-01-24 16:02:46 +0530

    Adding seed endpoint

--- project.py
+++ project.py
@@ -11,5 +11,21 @@ register_transform_classes(transforms)
 registry.write_transforms_config(include_output_entities=True)
 registry.write_settings_config()
 
-if __name__ == '__main__':
+
+@application.route("/seed/")
+def seed():
+    return {
+        "name": "Sample Maltego Integration",
+        "transforms": [
+            {
+                "id": "domain_to_ip",
+                "name": "Domain to IP",
+                "input": "maltego.Domain",
+                "endpoint": "/run/domain_to_ip",
+            }
+        ],
+    }
+
+
+if __name__ == "__main__":
     handle_run(__name__, sys.argv, application)
